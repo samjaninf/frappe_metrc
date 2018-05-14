6d4c5201f438f28dc64f37d4f4c0787fdb4be546
Neil Lasrado <neil@digithinkit.com>
2018-05-14 15:07:25 +0530

    Integrated Lab Test document with Metrc

--- frappe_metrc/frappe_metrc/doctype/lab_test/lab_test.py
+++ frappe_metrc/frappe_metrc/doctype/lab_test/lab_test.py
@@ -3,8 +3,39 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+
 import frappe
+import requests
 from frappe.model.document import Document
 
+#BASE_URL, AUTH, PARAMS = get_metrc_config()
+
 class LabTest(Document):
-	pass
+	def on_submit(self):
+		results = []
+		for res in self.results:
+			results.append({
+				"LabTestTypeName": res.lab_test_type_name,
+				"Quantity": res.quantity,
+				"Passed": res.passed,
+				"Notes": res.notes
+			})
+		
+		data = [
+			{
+				"Label": self.label,
+				"ResultDate": self.result_date,
+				"Results": results
+			}
+		]
+		
+		
+
+def get_metrc_config():
+	metrc_settings = frappe.get_single("Metrc API Settings")
+
+	base_url = metrc_settings.url
+	auth = (metrc_settings.api_key, metrc_settings.user_key)
+	params = {"licenseNumber": metrc_settings.lab_test}
+
+	return base_url, auth, params
